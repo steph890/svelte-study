0d2e3ee2ee2b5313692f3b85c71234795ba979d7
sanq <steph890@naver.com>
2022-03-06 15:19:23 +0900

    brainColor

---
+++ my-app/my-svelte-project/brainColor/src/routes/index.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { goto } from "$app/navigation";    // 화면 이동을 위한 goto 함수 사용 선언
+    import { scoreList } from "$lib/store.js";   // 점수 목록을 가져오기 위한 scoreList 함수 사용 선언
+</script>
+
+<svelte:head>
+    <title>Brain Color</title>
+</svelte:head>
+
+
+
+<h1 style="text-align:center;">두뇌 개발 게임</h1>
+
+<h3 style="text-align:center;">이번 주의 점수</h3>
+<table style="width:100%;">
+    <thead>
+        <tr>
+            <th>NO</th>
+            <th>점수</th>
+            <th>이름</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td style="text-align:center;">============</td>
+            <td style="text-align:center;">============</td>
+            <td style="text-align:center;">============</td>
+        </tr>
+  
+        {#each $scoreList as item ,index}
+            <tr>
+                <td style="text-align:center;">{index + 1}</td>
+                <td style="text-align:center;">{item.score}</td>
+                <td style="text-align:center;">{item.name}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+
+
+<p style="text-align:center;">
+    <button style="width:100%;height:60px;font-weight:bold;font-size:30px"
+      on:click={()=>{
+        goto("/quiz");
+      }}
+    >게임하기</button>
+</p>
---
+++ my-app/my-svelte-project/brainColor/src/routes/save.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { goto } from "$app/navigation";           // 화면 이동을 위한 goto 함수 사용 선언
+    import { score, scoreList } from "$lib/store.js"; // 스토어 변수 사용 선언
+
+    let name = "--";
+
+    function saveScore(){
+    $scoreList.push({ score: $score, name: name });
+    $scoreList.sort((a, b) => {return b.score - a.score});
+
+    $scoreList.pop();
+
+    goto("/");
+    }
+</script>
+
+<svelte:head>
+    <title>점수 저장하기</title>
+</svelte:head>
+
+<h1 style="text-align:center;">점수 저장하기</h1>
+
+<h4>이름을 입력하고 저장 버튼을 누르세요</h4>
+
+<p>획득점수: {$score}</p>
+
+<p>이름: <input type="text" bind:value={name} /></p>
+
+<button on:click={saveScore}> 저장 </button>
